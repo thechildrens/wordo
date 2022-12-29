97f2b44c7856a34b60a07fbcc1b388518901263f
Daimeng Chen <daimengchen@gmail.com>
2022-12-29 17:23:01 +0800

    fix del and etner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const GUESSES = new Array(6).fill(null).map(row => new Array(5).fill(''))
 
 const KEYBOARD = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-  ['⌫', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Enter'],
+  ['BACKSPACE', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'ENTER'],
 ]
 
+const KEY_CODES = {
+  'A': 97,
+  'B': 98,
+  'C': 99,
+  'D': 100,
+  'E': 101,
+  'F': 102,
+  'G': 103,
+  'H': 104,
+  'I': 105,
+  'J': 106,
+  'K': 107,
+  'L': 108,
+  'M': 109,
+  'N': 110,
+  'O': 111,
+  'P': 112,
+  'Q': 113,
+  'R': 114,
+  'S': 115,
+  'T': 116,
+  'U': 117,
+  'V': 118,
+  'W': 119,
+  'X': 120,
+  'Y': 121,
+  'Z': 122,
+  'Enter': 13,
+  'Backspace': 8,
+}
+
 function App() {
+  const [currRow, setCurrRow] = useState(0)
+
   useEffect(() => {
     const onkeydown = (evt) => {
       const key = evt.key.toUpperCase()
       const vkey = document.getElementsByClassName(`key-${key}`)[0]
+      if (vkey == null) return
       vkey.classList.add('key-active')
-      console.log(evt)
     }
     const onkeyup = (evt) => {
       const key = evt.key.toUpperCase()
       const vkey = document.getElementsByClassName(`key-${key}`)[0]
+      if (vkey == null) return
       vkey.classList.remove('key-active')
     }
 
@@ -31,7 +65,7 @@ function App() {
       window.removeEventListener('keydown', onkeydown)
       window.removeEventListener('keydown', onkeyup)
     }
-  }, [])
+  }, [currRow])
 
   return (
     <div className="App">
@@ -57,7 +91,11 @@ function App() {
           return (
             <div className="keyboard-row" key={row[0]}>
               {row.map((key) => {
-                return <div className={`key key-${key}`} key={key}>{key}</div>
+                return <div className={`key key-${key}`} key={key}>
+                  <span>
+                    {key}
+                  </span>
+                </div>
               })}
             </div>
           )
