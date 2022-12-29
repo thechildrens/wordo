e1f3814fdd1492c0b82332d71be6ceedc32375a7
Daimeng Chen <daimengchen@gmail.com>
2022-12-29 14:39:31 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+const GUESSES = new Array(6).fill(null).map(row => new Array(5).fill(''))
+
+const KEYBOARD = [
+  ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+  ['⌫', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Enter'],
+]
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="wordgrid">
+        <tbody>
+          {GUESSES.map((guess, i) => {
+            return (
+              <tr key={i}>
+                {guess.map((cell, j) => {
+                  return (
+                    <td key={j}>
+                      {cell}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="keyboard">
+        {KEYBOARD.map(row => {
+          return (
+            <div className="keyboard-row" key={row[0]}>
+              {row.map((key) => {
+                return <div className="key" key={key}>{key}</div>
+              })}
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
